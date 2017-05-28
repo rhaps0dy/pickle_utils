2d72f7a79b44e02fe404743a7a66f645fd0f7a34
Adrià Garriga <adria.garriga@gmail.com>
2017-05-28 22:42:24 +0100

    Made the module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pickle_utils',
+    packages = ['pickle_utils'],
+    version = '0.1',
+    description = 'Memoize functions to disk, load and save compressed Pickle files.',
+    author = 'Adrià Garriga-Alonso',
+    author_email = 'adria.garriga@gmail.com',
+    url = 'https://github.com/rhaps0dy/pickle_utils',
+    download_url = 'https://github.com/rhaps0dy/pickle_utils/archive/0.1.tar.gz',
+    keywords = ['pickle'], # arbitrary keywords
+    classifiers = [],
+)
+
